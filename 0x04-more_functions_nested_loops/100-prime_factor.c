927d9efce675acfe2fe2e76cc4c424b5cb04767f
chaimae <chaimaeaafif@gmail.com>
2023-08-20 17:18:29 -0700

    a

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -37,33 +37,3 @@ int main(void)
 
 	return (0);
 }
-
-
-101-print_number.c
-
-#include "main.h"
-
-/**
- * print_number - prints an integer
- * @n: integer to be printed
- */
-void print_number(int n)
-{
-	unsigned int n1;
-
-	if (n < 0)
-	{
-		n1 = -n;
-		_putchar('-');
-	} else
-	{
-		n1 = n;
-	}
-
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
-
-	_putchar((n1 % 10) + '0');
-}
