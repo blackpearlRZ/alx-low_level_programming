daeaab831d41e764db3ff34717db26cb7be4df55
chaimae <chaimaeaafif@gmail.com>
2023-08-28 16:23:24 -0700

    start feeling tired

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an
+ *	array of integers
+ * @a: the array
+ * @n: the number of elements of the array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, tmp, j = n - 1;
+
+	for (i = 0; i < j ; i++)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+		j--;
+	}
+}
