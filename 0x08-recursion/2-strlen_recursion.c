e8196820fb3eddb936305d0e5cd7df4390be4671
Chaimae <chaimaeaafif@gmail.com>
2023-09-05 22:27:27 +0000

    length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,9 +10,9 @@ int _strlen_recursion(char *s)
 {
 	int i = 0;
 
-	if (s[i] != '\0')
+	if (s[i] == '\0')
 	{
 		return (i);
 	}
-	_strlen_recursion(i + 1)
+	return (1 + _strlen_recursion(s + 1));
 }
