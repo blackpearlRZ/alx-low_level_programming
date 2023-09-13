f4c593851a5a13cfddd3bac229c788bec127d70b
Chaimae <chaimaeaafif@gmail.com>
2023-09-13 00:42:11 +0000

    init

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initializes a variable of type
+ *	struct dog*
+ * @d: pointer to struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
