543fed322f09b1c4a149304e8e080cc0a74c1a6e
Chaimae <chaimaeaafif@gmail.com>
2023-09-17 17:42:25 +0000

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,42 @@
+#include"main.h"
+
+/**
+ * _memset - fills memory with a constate byte
+ * @s: memory area
+ * @b: constant byte
+ * @n: the first bytes of the memoey area
+ * Return: pointer to the memory area s
+ */
+chae *-memset(char *s, char *b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0;  i < n ; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemeb: number of  elements of the array
+ * @size: size of each element
+ * Return: pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	arr = (void *)malloc(nmemb * size);
+
+	if (arr == NULL)
+		return (NULL);
+	_memset(arr, 0, nmemb * size);
+
+	return (arr);
+}
