b7010494361f7c7e3dae02a425227766efdbf808
chaimae <chaimaeaafif@gmail.com>
2024-01-10 22:01:03 +0100

    r

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *  * _strlen_recursion - returns the length of a string
- *   * @s: the string
- *    * Return: the length of string
- *     */
+ * _strlen_recursion - returns the length of a string
+ * @s: the string
+ * Return: the length of string
+ */
 
 int _strlen_recursion(char *s)
 {
