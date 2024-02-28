58e486559a87c263c7e5a41aebf6203c56a9015f
chaimae <chaimaeaafif@gmail.com>
2024-02-28 14:52:28 -0800

    clear

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * clear_bit - a function that sets the value
+ * of a bit to 0 at a given index
+ * @n: numea ptr
+ * @index: bit index
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
+}
